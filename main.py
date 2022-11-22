37976368a7591f0aac6c937a48ce4d519f82650f
victorsgdev <victorsg.dev@gmail.com>
2022-11-22 14:45:42 +0100

    "1.3"

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from numpy import var
 from statistics import mode
-from matplotlib import pyplot
+from matplotlib import pyplot as pp
 import sqlite3
 
 # Enlace : https://www.ine.es/jaxiT3/files/t/es/xls/32449.xls?nocab=1
@@ -42,5 +42,34 @@ def data_to_dic(col):
         dic_agricultura.append(("sector_agricultura", c, dataframe[c][1]))
 
 
-print(data_to_dic(columns))
 print(data_to_tupla(columns))
+print('\n')
+print(data_to_dic(columns))
+
+
+def concat_and_save(nombre, data):
+    file = open(nombre, "wt")
+    for values in data:
+        file.write(f"{values[1]}  {values[2]}\n")
+    print("Fichero creado e info almacenada correctamente.")
+
+
+concat_and_save("lista.txt", data_to_tupla(columns))
+
+dataframe2 = pd.read_excel(excel_file, sheet_name="tabla-32449")
+cambio_filas_columnas = dataframe2.transpose()
+print(cambio_filas_columnas)
+
+print(f"Media por año:\n{dataframe.mean()}")
+
+print(f"Varianza por año:\n{dataframe.var()}")
+
+
+def calcular_moda(col):
+    for i in col:
+        d = dataframe[i].tolist()
+        print(f"Moda año {i} {mode(d)}")
+
+
+calcular_moda(columns)
+
