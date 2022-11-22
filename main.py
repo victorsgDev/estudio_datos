56be09a1157e5123ea326a83b5792c4a8582fde6
victorsgdev <victorsg.dev@gmail.com>
2022-11-22 20:18:09 +0100

    comenzando sql

--- main.py
+++ main.py
@@ -60,9 +60,9 @@ dataframe2 = pd.read_excel(excel_file, sheet_name="tabla-32449")
 cambio_filas_columnas = dataframe2.transpose()
 print(cambio_filas_columnas)
 
-print(f"Media por año:\n{dataframe.mean()}")
+print(f"Media por año:\n{dataframe.mean(numeric_only=True)}")
 
-print(f"Varianza por año:\n{dataframe.var()}")
+print(f"Varianza por año:\n{dataframe.var(numeric_only=True)}")
 
 
 def calcular_moda(col):
@@ -72,4 +72,110 @@ def calcular_moda(col):
 
 
 calcular_moda(columns)
+
+
+def use_map(col):
+    df = dataframe[col].map(lambda prod: prod * 1.20).tolist()
+    print(df)
+
+
+use_map("2019")
+
+
+def values_greater_than_zero(col):
+    df = dataframe[col]
+    filt = filter(lambda prod: prod > 0, df)
+    print(list(filt))
+
+
+values_greater_than_zero("2020")
+
+
+class Data_table:
+    def __init__(self, sector):
+        self.sector = sector
+
+    def __str__(self):
+        sector = self.info()
+        return f"{self.sector} ha producido un total por año de: {str(sector)}"
+
+    def info(self):
+        sectores = dataframe["Producción"].tolist()
+        index = sectores.index(self.sector)
+        columnas = columns.delete(0)
+        tuple = []
+        sector = {}
+        for i in columnas:
+            sector[str(i)] = dataframe[str(i)][index]
+            tuple.append(dataframe[str(i)][index])
+        return sector, tuple
+
+    def __gt__(self, other):
+        result, _ = self.info()
+        if result.get("2021") > other:
+            return -1
+
+    def __eq__(self, other):
+        result, _ = self.info()
+        if result.get("2021") == other:
+            return 0
+
+    def __lt__(self, other):
+        result, _ = self.info()
+        if result.get("2021") < other:
+            return 1
+
+    def __add__(self, other):
+        result, _ = self.info()
+        return result.get("2021") + other
+
+    def __sub__(self, other):
+        result, _ = self.info()
+        return result.get("2021") - other
+
+
+pesca = Data_table("        03 Pesca y acuicultura")
+manufacturera = Data_table("        C Industria manufacturera")
+papel = Data_table("        17 Industria del papel")
+agricultura = Data_table("        A Agricultura, ganadería, silvicultura y pesca")
+quimica = Data_table("        20 Industria química")
+
+lista_sectores = [pesca, manufacturera, papel, agricultura, quimica]
+for ls in lista_sectores:
+    info, _ = ls.info()
+    info = info.get("2021")
+    print(ls.__gt__(info))
+    print(ls.__lt__(info))
+    print(ls.__eq__(info))
+    print(ls.__sub__(info))
+    print(ls.__add__(info))
+    print("-------------")
+
+
+def converter(data):
+    numbers = []
+    for x in data:
+        numbers.append(int(x))
+    return numbers
+
+
+fig, ax = pp.subplots()
+_, ax2 = pp.subplots()
+_, ax3 = pp.subplots()
+
+years = columns.delete(0)
+pesca = Data_table("        03 Pesca y acuicultura")
+_, prod = pesca.info()
+lista = converter(prod)
+ax.plot(years, prod, color='red')
+pp.savefig('grafico.png')
+ax2.scatter(years, prod)
+ax3.bar(years, prod)
+pp.show()
+
+
+con = sqlite3.connect("produccion.db")
+
+table = dataframe.to_sql(name="Produccion", con=con)
+
 
